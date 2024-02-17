c73873a71f14e01e6f873fd41cc545bf574bd5af
Kevin Kabore <kevin.s.kabore@gmail.com>
2024-02-16 21:16:31 -0500

    adds base pages

---
+++ pages/blog..tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogPage: React.FC = () => {
+  return (
+    <div>
+      <h1>This is the Blog</h1>
+      <p>Kevin has an awesome Blog.</p>
+    </div>
+  )
+}
+
+export default BlogPage
