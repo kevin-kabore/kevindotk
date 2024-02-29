92ca76e3c0ab8d46464679704bb6fdc9c9e363f7
Kevin Kabore <kevin.s.kabore@gmail.com>
2024-02-29 12:31:11 -0500

    adds auth models

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import {PrismaAdapter} from '@next-auth/prisma-adapter'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export const authOptions = {
   providers: [
@@ -8,6 +12,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
   ],
+  adapter: PrismaAdapter(prisma),
 }
 
 export default NextAuth(authOptions)
