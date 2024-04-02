671107b22f5f39448a55c550924ab3d027651658
Kevin Kabore <kevin.s.kabore@gmail.com>
2024-04-02 09:48:46 -0400

    remove privy imports, refactor providers

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import Link from 'next/link'
 import {signIn, signOut, useSession} from 'next-auth/react'
-import {usePrivy} from '@privy-io/react-auth'
 
 import DropdownMenu from './DropdownMenu'
 
@@ -33,8 +32,6 @@ const HomeDropdownMenu: React.FC = () => {
 }
 const NavBar: React.FC = () => {
   const {data: session} = useSession()
-  const privy = usePrivy()
-  const {login, logout, authenticated, ready, user} = privy
 
   return (
     <nav className="bg-gradient-to-r from-gray-800 via-gray-900 to-black text-white py-4 px-6 z-50 flex justify-between items-center border-solid">
@@ -52,11 +49,6 @@ const NavBar: React.FC = () => {
         <Link href="/blog">Blog</Link>
       </div>
       {/* Right section */}
-      {authenticated ? (
-        <button onClick={() => logout()}>log out</button>
-      ) : (
-        <button onClick={() => login()}>login</button>
-      )}
       <div className="flex items-center space-x-4">
         {session ? (
           <button className="hover:text-gray-300" onClick={() => signOut()}>
